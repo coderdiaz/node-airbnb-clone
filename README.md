9cee8466c0e8736bb4af5a27f848fcae7d4de9ad
Javier Diaz <coderdiaz@gmail.com>
2018-08-05 19:23:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Airbnb Clone
+## A simple node application based on Airbnb.com.
+
+### Used tools
+- Node.
+- Express.js.
+- MongoDB & Mongoose.
+- Jade for templating.
+
+### Development
+```sh
+# Install dependencies.
+$ yarn install
+
+# Start project.
+$ yarn start
+
+# Go to localhost on port 3000 in a browser. Note: If you specified a custom port on environment go to it.
+```
+
+Made with fingers and pizzas.
---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var createError = require('http-errors');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+// You can use another render template engine. Ex. ejs, hbs, jade, dust.
+app.set('view engine', 'jade');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only prividing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
