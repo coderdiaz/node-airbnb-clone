aad5021b2b2f515adca876b972c42c79608408ff
Javier Diaz <coderdiaz@gmail.com>
2018-08-13 23:57:21 -0500

    Added routes for API

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require('express');
 var path = require('path');
 var createError = require('http-errors');
 
-// Routes
+// Pages
 var indexRouter = require('./routes/index');
+
+// API
+var houseApiRouter = require('./routes/api/house');
 
 var app = express();
 
@@ -17,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api/house', houseApiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -29,9 +33,17 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  const statusCode = err.status || 500;
+  // render error on API
+  if (/.api/.test(req.url)) {
+    return res.status(statusCode).json({
+      code: statusCode,
+      message: err.message
+    });
+  }
+
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  return res.status(statusCode).render('error');
 });
 
 module.exports = app;
